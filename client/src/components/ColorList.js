8922148cc76aee8c47673d7b7ca86a1d2ba0a017
Blair <abrgroup@gmail.com>
2020-03-22 20:51:53 -0400

    MVP complete

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
 const initialColor = {
   color: "",
@@ -18,13 +19,28 @@ const ColorList = ({ colors, updateColors }) => {
 
   const saveEdit = e => {
     e.preventDefault();
+    console.log("colortoedit id", colorToEdit.id);
     // Make a put request to save your updated color
     // think about where will you get the id from...
     // where is is saved right now?
+    axiosWithAuth()
+      .put(`api/colors/${colorToEdit.id}`, colorToEdit)
+      .then(() => {
+        setEditing(false);
+        window.location.href = "http://localhost:3000/protected";
+      })
+      .catch(err => console.log(err));
   };
 
   const deleteColor = color => {
     // make a delete request to delete this color
+    axiosWithAuth()
+      .delete(`/api/colors/${color.id}`)
+      .then(res => {
+        console.log(res);
+        window.location.href = "http://localhost:3000/protected";
+      })
+      .catch(err => console.log(err));
   };
 
   return (
@@ -34,12 +50,14 @@ const ColorList = ({ colors, updateColors }) => {
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
             <span>
-              <span className="delete" onClick={e => {
-                    e.stopPropagation();
-                    deleteColor(color)
-                  }
-                }>
-                  x
+              <span
+                className="delete"
+                onClick={e => {
+                  e.stopPropagation();
+                  deleteColor(color);
+                }}
+              >
+                x
               </span>{" "}
               {color.color}
             </span>
--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,12 +1,60 @@
-import React from "react";
+import React, { useState } from "react";
 
-const Login = () => {
+import { axiosWithAuth } from "../utils/axiosWithAuth";
+
+const Login = props => {
   // make a post request to retrieve a token from the api
   // when you have handled the token, navigate to the BubblePage route
+  const [user, setUser] = useState({
+    credentials: {
+      username: "",
+      password: ""
+    }
+  });
+
+  const handleChange = e => {
+    setUser({
+      credentials: {
+        ...user.credentials,
+        [e.target.name]: e.target.value
+      }
+    });
+  };
+
+  const onSubmit = e => {
+    e.preventDefault();
+    axiosWithAuth()
+      .post("/api/login", user.credentials)
+      .then(res => {
+        console.log(res);
+        localStorage.setItem("token", res.data.payload);
+        props.history.push("/protected");
+      })
+      .catch(err => console.log(err));
+  };
+
   return (
     <>
       <h1>Welcome to the Bubble App!</h1>
-      <p>Build a login page here</p>
+      <div>
+        <form onSubmit={onSubmit}>
+          <input
+            type="text"
+            name="username"
+            placeholder="username"
+            value={user.credentials.username}
+            onChange={handleChange}
+          />
+          <input
+            type="password"
+            name="password"
+            placeholder="password"
+            value={user.credentials.password}
+            onChange={handleChange}
+          />
+          <button>Log in</button>
+        </form>
+      </div>
     </>
   );
 };
